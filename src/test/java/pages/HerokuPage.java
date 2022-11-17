b23552c66c54831788928656883c669d6f82914a
AFomina <alonafomina3@gmail.com>
2022-11-17 13:46:57 -0600

    ...

---
+++ src/test/java/pages/HerokuPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.Driver;
+
+import java.util.List;
+
+public class HerokuPage {
+
+
+}
---
+++ src/test/java/steps/HerokuSteps.java
@@ -0,0 +1,16 @@
+package steps;
+
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import pages.HerokuPage;
+import utils.ActionsUtil;
+import utils.Driver;
+import utils.ValidationUtils;
+
+public class HerokuSteps {
+
+}
